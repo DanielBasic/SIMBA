358d3ab83e7af99c1411d3c51bc6223c20b611ed
Jefferson Sousa <jeffersonluisdesousa1@gmail.com>
2023-07-31 18:05:27 -0300

    Corrigido template

--- simba/settings.py
+++ simba/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'search',
 ]
 
@@ -138,6 +139,3 @@ EMAIL_HOST_PASSWORD = 'pcasjrejggsmuqqv'
 
 
 
-LOGIN_URL = 'novo_login/'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = 'novo_login/'
