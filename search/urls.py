ae7af6ef9b185fc5328fd01ca6c96636a4f89ce4
Jefferson Sousa <jeffersonluisdesousa1@gmail.com>
2023-08-09 21:30:00 -0300

    teste  2

--- search/urls.py
+++ search/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-  path("search", views.search, name="search"),
+  path("", views.search, name="search"),
   path('add_product/', views.add_product, name='add_product'),
   path('gerenciar_agrupamento/', views.gerenciar_agrupamento, name='gerenciar_agrupamento'),
 
