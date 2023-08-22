6bdc25fbfe71f0564c6c640eadeb1573ef82af89
Daniel <danielpereira12002@gmail.com>
2023-08-22 13:15:30 -0300

    Implementation: Select all button, add group_by_ad button

--- templates/static/search/js/search.js
+++ templates/static/search/js/search.js
@@ -1,3 +1,64 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var selectAllCheckbox = document.getElementById("select-all-products");
+  var checkboxes = document.querySelectorAll(".checkbox_product");
+
+  selectAllCheckbox.addEventListener("change", function() {
+    checkboxes.forEach(function(checkbox) {
+      checkbox.checked = selectAllCheckbox.checked;
+    });
+  });
+
+  checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener("change", function() {
+      if (!checkbox.checked) {
+        selectAllCheckbox.checked = false;
+      } else {
+        var allChecked = true;
+        checkboxes.forEach(function(item) {
+          if (!item.checked) {
+            allChecked = false;
+          }
+        });
+        selectAllCheckbox.checked = allChecked;
+      }
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var checkboxes = document.querySelectorAll(".checkbox_product");
+  var myButton = document.getElementById("set_products");
+
+  checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener("change", function() {
+      var anyChecked = false;
+      checkboxes.forEach(function(item) {
+        if (item.checked) {
+          anyChecked = true;
+        }
+      });
+
+      if (anyChecked) {
+        myButton.style.display = "block";
+      } else {
+        myButton.style.display = "none";
+      }
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var floatingButton = document.getElementById("floating-button");
+
+  window.addEventListener("scroll", function() {
+    if (window.scrollY > 200) {  // Adjust the scroll threshold as needed
+      floatingButton.style.display = "block";
+    } else {
+      floatingButton.style.display = "none";
+    }
+  });
+});
+
 
 $(document).ready(function() {
     $('.filter_to_exclude_button').click(function() {
