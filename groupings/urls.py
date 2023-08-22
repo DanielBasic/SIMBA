8b25c0c1c6d46e20cb4a7fe5894d27af2b3de824
Jefferson Sousa <jeffersonluisdesousa1@gmail.com>
2023-08-21 21:17:42 -0300

    criado o agrupamento dos sellers, ajustes no filtro do html index

--- groupings/urls.py
+++ groupings/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("gerenciar_agrupamentos/", views.gerenciar_agrupamentos, name="gerenciar_agrupamentos"),
-  path("criar_agrupamentos/", views.criar_agrupamentos, name="criar_agrupamentos"),
-  
+  path("gerenciar_agrupamentos_produtos/", views.gerenciar_agrupamentos_produtos, name="gerenciar_agrupamentos_produtos"),
+  path("criar_agrupamentos_produtos/", views.criar_agrupamentos_produtos, name="criar_agrupamentos_produtos"),
+  path("gerenciar_agrupamentos_seller/", views.gerenciar_agrupamentos_seller, name="gerenciar_agrupamentos_seller"),
+  path("criar_agrupamentos_seller/", views.criar_agrupamentos_seller, name="criar_agrupamentos_seller"),
 ]
--- groupings/views.py
+++ groupings/views.py
@@ -10,9 +10,9 @@ from .models import Agrupamento
 # Create your views here.
 
 @login_required
-def criar_agrupamentos(request):
+def criar_agrupamentos_produtos(request):
   if request.method == "GET":
-    return render(request, "groupings/criar_agrupamentos.html")
+    return render(request, "groupings/criar_agrupamentos_produtos.html")
   elif request.method == "POST":
     logo = request.POST.get("logo")
     title  = request.POST.get("title")
@@ -30,17 +30,27 @@ def criar_agrupamentos(request):
 
     messages.add_message(request, constants.SUCCESS,'Evento cadastrado com sucesso')
                             
-    return redirect(reverse('criar_agrupamentos'))
+    return redirect(reverse('criar_agrupamentos_produtos'))
   
 
     
 @login_required
-def gerenciar_agrupamentos(request):
+def gerenciar_agrupamentos_produtos(request):
   if request.method == "GET":
     grupos = Agrupamento.objects.filter(criador=request.user)
-    return render(request, "groupings/gerenciar_agrupamentos.html", {'agrupamentos':grupos })
+    return render(request, "groupings/gerenciar_agrupamentos_produtos.html", {'agrupamentos':grupos })
 
 
 def deletar_agrupamentos(request):
+  pass
+  
+
+def gerenciar_agrupamentos_seller(request):
+  if request.method == "GET":
+    grupos = Agrupamento.objects.filter(criador=request.user)
+    return render(request, "groupings/gerenciar_agrupamentos_seller.html", {'agrupamentos':grupos })
+
+
+def criar_agrupamentos_seller(request):
   pass
 
