aac2c4619709e8686e7c6943697c5d54df533369
jefferson Sousa <jeffersonluisdesousa1@gmail.com>
2023-09-16 11:12:42 -0300

    alterado o modo de visualização de groups,  estilizado new group,  realizada a troca das fontes por icones

--- simba/settings.py
+++ simba/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--*$wgs30dp!831biex84_*=ys_((lo&us!1#sm67tbg5(=7gmu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
