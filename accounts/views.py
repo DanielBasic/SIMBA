b36839e1d89f27247e85b92af4a8ba8656d747e5
DanielBasic <danielpereira12002@gmail.com>
2023-09-22 11:52:04 -0300

    validiation in sing-up with js, aplication with celery[redis]

--- accounts/views.py
+++ accounts/views.py
@@ -4,11 +4,18 @@ from django.contrib.messages import constants
 from django.http import HttpResponse
 from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 import logging
 
 def login(request):
   if request.method == "GET":
-    return render(request, "registration/login.html")
+    if  request.user.is_authenticated:
+      return redirect(reverse("groupByAd_management"))  
+    else:
+      return render(request, "registration/login.html")
   
   elif request.method == "POST":
     email_nickname = request.POST.get("email-username")
@@ -27,8 +34,7 @@ def login(request):
         return redirect(reverse("login"))
     
     auth.login(request, user)
-
-    return redirect(reverse("search"))
+    return redirect(reverse("groupByAd_management"))
     
 import json
 from django.http import JsonResponse
@@ -101,6 +107,25 @@ def logout(request):
   return redirect(reverse("login"))
     
 
+
+
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         UserProfile.objects.create(user=instance)
+
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.userprofile.save()
+
 def sobre(request):
   if request.method == "GET":
     return render(request, "accounts/sobre.html")
+
+    # endereco = request.user.userprofile.endereco
+    # telefone = request.user.userprofile.telefone
+    # data_de_nascimento = request.user.userprofile.data_de_nascimento    
+    
+    # return render(request, "accounts/sobre.html", {endereco:endereco, telefone:telefone, data_de_nascimento:data_de_nascimento})
+  
+
