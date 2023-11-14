ffe60b7a529bf8ee2282099fcfb5f80126bd7dad
DanielBasic <danielpereira12002@gmail.com>
2023-11-14 14:48:20 -0300

    fixing api errors

--- product/views.py
+++ product/views.py
@@ -14,14 +14,13 @@ def index(request):
         app_id = config('APP_ID')
         client_secret = config('CLIENT_SECRET')
         refresh_token = config('REFRESH_TOKEN')
-        
         access_token = get_access_token(app_id, client_secret, refresh_token)
-        access_token = access_token["access_token"]   
+
         # key_word = request.GET.get("keyWord")
         product_id = request.GET.get("product_id")
         # products = Product.objects.filter(Q(id=product_id) & Q(user=request.user)).all()
         if product_id:
-            product_info = getInfoFromProduct(access_token, product_id)
+            product_info = getInfoFromProduct(access_token['access_token'], product_id)
             if product_info:
        
                 return render(request, "product/index.html", {'product' : product_info})
