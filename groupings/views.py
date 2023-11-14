6f62df18063a3e8ed5cefaa705690b22d67a000b
jefferson Sousa <jeffersonluisdesousa1@gmail.com>
2023-11-14 14:53:44 -0300

    merge

--- groupings/views.py
+++ groupings/views.py
@@ -283,27 +283,27 @@ from decouple import config
 from django.contrib.auth.decorators import login_required
 
 
-def index(request, product_id):
-  if request.method == "GET":
-      app_id = config('APP_ID')
-      client_secret = config('CLIENT_SECRET')
-      refresh_token = config('REFRESH_TOKEN')
+# def index(request, product_id):
+#   if request.method == "GET":
+#       app_id = config('APP_ID')
+#       client_secret = config('CLIENT_SECRET')
+#       refresh_token = config('REFRESH_TOKEN')
       
-      access_token = get_access_token(app_id, client_secret, refresh_token)
-      access_token = access_token["access_token"]   
-      # key_word = request.GET.get("keyWord")
-      product_id = request.GET.get("product_id")
-      # products = Product.objects.filter(Q(id=product_id) & Q(user=request.user)).all()
-      if product_id:
-          product_info = getInfoFromProduct(access_token, product_id)
-          if product_info:
+#       access_token = get_access_token(app_id, client_secret, refresh_token)
+#       access_token = access_token["access_token"]   
+#       # key_word = request.GET.get("keyWord")
+#       product_id = request.GET.get("product_id")
+#       # products = Product.objects.filter(Q(id=product_id) & Q(user=request.user)).all()
+#       if product_id:
+#           product_info = getInfoFromProduct(access_token, product_id)
+#           if product_info:
       
-              return render(request, "product/index.html", {'product' : product_info})
+#               return render(request, "product/index.html", {'product' : product_info})
 
-      return render(request, "product/index.html")
+#       return render(request, "product/index.html")
 
-  elif request.method == "POST":
-      return render(request, "product/index.html")
+#   elif request.method == "POST":
+#       return render(request, "product/index.html")
   
 
 
